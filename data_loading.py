3507ad1237f3f75a5edfd5ca09c9995dee44be3d
ayushpatidar <ayush.patidar4@gmail.com>
2018-11-28 21:42:36 +0530

    command line data reading added

--- data_loading.py
+++ data_loading.py
@@ -1,17 +1,40 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
-
-#if file is csv
-#df = pd.read_csv()
-
-#if file is json
-
-df = pd.read_json("Datasets/News_Category_Dataset.json", lines=True)
-
-#first 5 rows of dataframe are
-print(df.head())
+import argparse
+import warnings
 
 
-#columns of datframe are..
-print(df.columns)
+warnings.filterwarnings("ignore")
+
+class load_data():
+
+    def read_data(self, file_name):
+        #if file is csv
+        #df = pd.read_csv()
+
+        #if file is json
+
+        df = pd.read_json("Datasets/"+file_name, lines=True)
+
+        #first 5 rows of dataframe are
+        print(df.head())
+
+
+        #columns of datframe are..
+        print(df.columns)
+
+
+
+
+parser = argparse.ArgumentParser(description="modelling")
+parser.add_argument('--file_name', type=str, help="reading_file_name")
+args = parser.parse_args()
+
+
+
+
+object_load = load_data()
+
+object_load.read_data(args.file_name)
+
